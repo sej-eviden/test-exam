3acb5151a7cd6a0eb48d83c5c23b2fa0de243c83
sergio-esteve-atos <sergio.esteve@atos.net>
2023-07-06 14:47:09 +0200

    OneCloud spain + typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['Montserrat', 'ui-sans-serif', 'system-ui']
+		},
 		extend: {
 			colors: {
 				'coral': '#ff6d43',
